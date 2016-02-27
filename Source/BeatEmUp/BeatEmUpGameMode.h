3ce3f0c45a49e38f83e3bd0bea4e2aca496dd3ab
Matt LeBrun <lebrun@adobe.com>
2016-02-26 21:17:44 -0500

    Initial Commit

---
+++ Source/BeatEmUp/BeatEmUpGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BeatEmUpGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEATEMUP_API ABeatEmUpGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
